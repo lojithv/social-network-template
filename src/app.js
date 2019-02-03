adb013db22597333e0d598839e5e55f15388a05a
Alberta Williams <alberta04@gmail.com>
2019-02-03 15:40:58 -0500

    moved frontend code

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.set('port', process.env.PORT || 3000);
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'frontend-react/build')));
+app.use(express.static(path.join(__dirname, '../frontend-react/build')));
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
